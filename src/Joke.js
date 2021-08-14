3505ba68a37755520c3727300e8fcbc2c5bc4a4d
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-14 16:15:00 -0700

    implemented the Joke class

--- src/Joke.js
+++ src/Joke.js
@@ -1,40 +1,62 @@
 import React from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {
-  const upVote = () => vote(id, +1);
-  const downVote = () => vote(id, -1);
+// function Joke({ vote, votes, text, id }) {
+//   const upVote = () => vote(id, +1);
+//   const downVote = () => vote(id, -1);
 
-  return (
-    <div className="Joke">
-      <div className="Joke-votearea">
-        <button onClick={upVote}>
-          <i className="fas fa-thumbs-up" />
-        </button>
+//   return (
+//     <div className="Joke">
+//       <div className="Joke-votearea">
+//         <button onClick={upVote}>
+//           <i className="fas fa-thumbs-up" />
+//         </button>
 
-        <button onClick={downVote}>
-          <i className="fas fa-thumbs-down" />
-        </button>
+//         <button onClick={downVote}>
+//           <i className="fas fa-thumbs-down" />
+//         </button>
 
-        {votes}
-      </div>
+//         {votes}
+//       </div>
 
-      <div className="Joke-text">{text}</div>
-    </div>
-  );
-}
+//       <div className="Joke-text">{text}</div>
+//     </div>
+//   );
+// }
 
 class Joke extends React.Component {
   constructor(props) {
     super(props);
+    this.upVote = this.upVote.bind(this);
+    this.downVote = this.downVote.bind(this);
   }
 
   upVote() {
-    vote(id, +1);
+    this.props.vote(this.props.id, +1);
   }
 
   downVote() {
-    vote(id, -1);
+    this.props.vote(this.props.id, -1);
+  }
+
+  render() {
+    return (
+      <div className="Joke">
+        <div className="Joke-votearea">
+          <button onClick={this.upVote}>
+            <i className="fas fa-thumbs-up" />
+          </button>
+
+          <button onClick={this.downVote}>
+            <i className="fas fa-thumbs-down" />
+          </button>
+
+          {this.props.votes}
+        </div>
+
+        <div className="Joke-text">{this.props.text}</div>
+      </div>
+    )
   }
 }
 
